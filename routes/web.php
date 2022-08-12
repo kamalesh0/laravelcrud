2a02fba293eb3a22712e4947974ce8d655fe1c48
KAMALESH R <95736411+kamalesh0@users.noreply.github.com>
2022-08-12 15:05:38 +0530

    hlo commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+  
+use Illuminate\Support\Facades\Route;
+  
+use App\Http\Controllers\Auth\AuthController;
+  
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+  
+Route::get('login', [AuthController::class, 'index'])->name('login');
+Route::post('post-login', [AuthController::class, 'postLogin'])->name('login.post'); 
+Route::get('registration', [AuthController::class, 'registration'])->name('register');
+Route::post('post-registration', [AuthController::class, 'postRegistration'])->name('register.post'); 
+Route::get('companies', [AuthController::class, 'companies']); 
+Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+use App\Http\Controllers\CompanyCRUDController;
+ 
+Route::resource('companies', CompanyCRUDController::class);
+Route::get('/', function () {
+    return view('welcome');
+});
